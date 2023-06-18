fce383da259c1d7e0cf2b44d6793b6583991d1bb
Oein <oein0219@gmail.com>
2023-06-18 11:51:36 +0900

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,72 @@ const { terser } = require("rollup-plugin-terser");
 const external = require("rollup-plugin-peer-deps-external");
 const postcss = require("rollup-plugin-postcss");
 const dts = require("rollup-plugin-dts").default;
+const fs = require("fs");
 
 const packageJson = {
   main: "dist/cjs/index.js",
   module: "dist/esm/index.js",
   types: "dist/index.d.ts",
+};
+
+const getFolders = (entry) => {
+  // get the names of folders and files of the entry directory
+  let dirs = fs.readdirSync(entry);
+  // ['Accordion', 'Button'...]
+  dirs = dirs.map((j) => j.replace(".tsx", ""));
+  return dirs;
+};
+
+//loop through your folders and generate a rollup obj per folder
+const folderBuilds = () => {
+  return getFolders("./src/components").map((component) => {
+    return {
+      input: `src/components/${component}/index.tsx`,
+      output: [
+        {
+          file: "dist/cjs/components/" + component + "/index.js",
+          format: "cjs",
+          sourcemap: false,
+          name: "react-ts-lib",
+        },
+        {
+          file: "dist/esm/components/" + component + "/index.js",
+          format: "esm",
+          sourcemap: false,
+        },
+      ],
+      plugins: [
+        external(),
+        resolve(),
+        commonjs(),
+        typescript({ tsconfig: "./tsconfig.json" }),
+        postcss({}),
+        terser(),
+      ],
+      external: ["react", "react-dom"],
+    };
+  });
+};
+
+//loop through your folders and generate a rollup obj per folder
+const folderTypes = () => {
+  return getFolders("./src/components").map((component) => {
+    return {
+      input: `src/components/${component}/index.tsx`,
+      output: [
+        {
+          file: "dist/esm/components/" + component + "/index.d.ts",
+          format: "esm",
+        },
+        {
+          file: "dist/cjs/components/" + component + "/index.d.ts",
+          format: "esm",
+        },
+      ],
+      external: [/\.css$/],
+      plugins: [dts()],
+    };
+  });
 };
 
 exports.default = [
@@ -36,11 +97,18 @@ exports.default = [
       postcss({}),
       terser(),
     ],
+    external: ["react", "react-dom"],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: "src/index.ts",
+    output: [
+      { file: "dist/index.d.ts", format: "esm" },
+      { file: "dist/cjs/index.d.ts", format: "cjs" },
+      { file: "dist/esm/index.d.ts", format: "esm" },
+    ],
     external: [/\.css$/],
     plugins: [dts()],
   },
+  ...folderBuilds(),
+  ...folderTypes(),
 ];
